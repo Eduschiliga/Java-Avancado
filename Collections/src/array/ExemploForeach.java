46bbff86266c13a407d7ea3c6b15778198ec1898
Eduardo Schiliga <eschiliga@alunos.utfpr.edu.br>
2023-07-27 12:36:10 -0300

    Atualizando

---
+++ Collections/src/array/ExemploForeach.java
@@ -0,0 +1,12 @@
+package array;
+
+public class ExemploForeach {
+	public static void main(String[] args) {
+		double[] notas = { 9.5, 2, 6.9, 7.2, 10, 7.8 };
+
+		for (double nota : notas) {
+			System.out.println(nota);
+		}
+
+	}
+}

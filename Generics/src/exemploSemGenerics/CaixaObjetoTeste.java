430e666b934a8c94d0bfbf628804ff75ad01ecfe
Eduardo Schiliga <eschiliga@alunos.utfpr.edu.br>
2023-08-13 13:47:39 -0300

    Agora vai

---
+++ Generics/src/exemploSemGenerics/CaixaObjetoTeste.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exemploSemGenerics;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class CaixaObjetoTeste {
+
+	public static void main(String[] args) {
+		CaixaObjeto caixaA = new CaixaObjeto();
+
+		caixaA.guardar(2.5); // double -> Double
+
+		CaixaObjeto caixaB = new CaixaObjeto();
+		caixaB.guardar("Hello, World!");
+
+		Double coisaA = (Double) caixaA.abrir();
+		System.out.println(coisaA);
+
+		String coisaB = (String) caixaB.abrir();
+		System.out.println(coisaB);
+
+	}
+
+}
